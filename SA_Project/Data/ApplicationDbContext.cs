5e6794f8c054a502bbc1ff30da54e1c3e79014d4
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-09-15 01:04:04 +0300

    add connection string

--- SA_Project/Data/ApplicationDbContext.cs
+++ SA_Project/Data/ApplicationDbContext.cs
@@ -1,15 +1,16 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SA_Project.Models;
 
 namespace SA_Project.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
         }
 
-
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     }
 }
---
+++ SA_Project/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SA_Project.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Name { get; set; }
+    }
+}
