d64a80613092668dcacc18050da12b5e9f5d4037
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-09-15 03:44:39 +0300

    create IOrderRepository and OrderRepository

---
+++ SA_Project/Repository/OrderRepository.cs
@@ -0,0 +1,20 @@
+﻿using SA_Project.Data;
+using SA_Project.Models.Order;
+using SA_Project.Repository.IRepository;
+
+namespace SA_Project.Repository
+{
+    public class OrderRepository : Repository<Order>, IOrderRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public OrderRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Update(Order order)
+        {
+            _db.Update(order);
+        }
+    }
+}
