5b9ee5cb39af20ae3d7eb5f8464a94fb1d0d4677
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-09-16 23:24:00 +0300

    create frontend project

---
+++ SA_Project.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SA_Project.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
